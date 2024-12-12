fe0d49978c650258e4353e42b94feb30bc0a268c
mohdanas-2002 <1manas7396@gmail.com>
2024-12-12 12:27:59 +0000

    code merge

---
+++ sample1.c
@@ -0,0 +1,15 @@
+gcc HelloWorld.c -o HelloWorld
+./HelloWorld
+gcc HelloWorld.c -o HelloWorld
+./HelloWorld
+gcc HelloWorld.c -o HelloWorld
+./HelloWorld
+gcc HelloWorld.c -o HelloWorld
+./HelloWorld
+gcc HelloWorld.c -o HelloWorld
+./HelloWorld
+gcc HelloWorld.c -o HelloWorld
+./HelloWorld
+gcc HelloWorld.c -o HelloWorld
+./HelloWorld
+
